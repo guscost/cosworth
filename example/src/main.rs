28998e60efc67684cacd60a4fc4577842b33ecdd
Gus Cost <guscost@gmail.com>
2018-09-02 14:08:32 -0400

    example project

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,9 @@
 extern crate actix_web;
 
 #[macro_use]
+extern crate diesel;
+
+#[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 extern crate serde;
@@ -11,24 +14,18 @@ extern crate cosworth;
 use actix_web::{http, pred, server, App, Error, HttpRequest, HttpResponse};
 use cosworth::response::json;
 
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Widget {
-    id: String,
-    name: String,
-}
-
+mod models;
 
 fn index(req: &HttpRequest) -> Result<HttpResponse, Error> {
-    let id = req.match_info().query("id")?;
+    let id = 234;
     let name = req.match_info().query("name")?;
 
-    if id == "123" {
+    if id == 123 {
         return Ok(req.build_response(http::StatusCode::PARTIAL_CONTENT)
               .content_type("text/plain")
               .body(hello!()));
     } else {
-        let widget = Widget { id: id, name: name };
+        let widget = models::todo::Todo { id: id, name: name, done: false };
         return Ok(json(&req, widget, http::StatusCode::OK)?);
     }
 }
---
+++ example/src/models/todo.rs
@@ -0,0 +1,15 @@
+//use diesel::prelude::*;
+
+#[derive(Insertable)]
+#[table_name="todos"]
+pub struct NewTodo<'a> {
+    pub name: &'a str,
+}
+
+#[derive(Queryable)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Todo {
+    pub id: i32,
+    pub name: String,
+    pub done: bool,
+}
